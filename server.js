8efd89df2bda396e44e1057343039f3f3d5df4b3
jyoungcode <jyoungcode@gmail.com>
2016-09-02 16:04:01 +0900

    broadcast

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.set("port", process.env.PORT || 3030);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+io.on("connection", function(socket){
+    socket.on("chat message", function(msg){
+        io.emit("chat message", msg);
+    });
+});
+
+http.listen(app.get("port"), function(){
+    console.log("Express chat server is running at localhost:" + app.get("port"));
+});
+
+
+// console.log("a user connection");
+	// socket.on("disconnect", function(){
+	// 	console.log("user disconnected");
+	// });
